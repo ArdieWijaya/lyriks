0a3dfe006798738334c5b3831794c006ccefad9b
ArdieWijaya <ArdieWijaya@users.noreply.github.com>
2022-10-04 20:24:50 +0700

    initial commit

---
+++ src/pages/Discover.jsx
@@ -0,0 +1,11 @@
+import { Error, Loader, SongCard } from "../components";
+import { genres } from "../assets/constants";
+
+const Discover = () => {
+    console.log(genres);
+    const genreTitle = "Pop";
+
+    return <div></div>;
+};
+
+export default Discover;
